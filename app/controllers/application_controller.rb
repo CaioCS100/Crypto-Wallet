cb3184f892fb1096c811f88b7af9cdefd59eca09
Caio Cesar Silva <caio.cesar.s100@gmail.com>
2019-12-30 12:17:47 -0300

    Adding in the system a link that a user can change the language between portuguese and english

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    if verify_locale(params[:locale])
+      cookies[:locale] = params[:locale]
+    end
+
+    if cookies[:locale] && I18n.locale != cookies[:locale]
+      I18n.locale = cookies[:locale]
+    end
+  end
+
+  def verify_locale(locale = nil)
+    ['pt-BR', 'en'].include?(locale) && locale ? true : false
+  end
 end
